695b4a78f8f124184433b5833701fbfd2473743b
Thomas Marckx <thomas.marckx@optis.be>
2016-10-01 14:50:02 +0200

    initital commit

---
+++ contentconsumer/contentconsumer.controller.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('ContentConsumerController', ContentConsumerController);
+
+        ContentConsumerController.$inject = ['$rootScope'];
+        function ContentConsumerController($rootScope) {
+
+        }
+})();
---
+++ contentprovider/contentprovider.controller.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('ContentProviderController', ContentProviderController);
+
+        ContentProviderController.$inject = ['$rootScope'];
+        function ContentProviderController($rootScope) {
+
+        }
+})();
